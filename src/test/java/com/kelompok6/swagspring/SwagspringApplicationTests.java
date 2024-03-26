8ba87b8fe6ba61bf738746f19b9f610e1b6e2b3f
ryuuwiz <ryuuwiz@gmail.com>
2024-03-26 23:14:05 +0700

    initial project

---
+++ src/test/java/com/kelompok6/swagspring/SwagspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kelompok6.swagspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwagspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
